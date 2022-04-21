93d5eb0343cb7ec8f88cd10d784deeb41d5456db
GitArts <44618751+GitArts@users.noreply.github.com>
2022-04-21 16:10:14 +0300

    nExt

--- Next_try_OD.py
+++ Next_try_OD.py
@@ -1,57 +1,9 @@
 import open3d as o3d
 import numpy as np
-import sys; sys.path.append("../../../kopigs")
+import sys; sys.path.append("../../../../kopigs")
 import heapq
 from My_Functions import _get_pcd, show_pcd, Find_corner
 from My_Functions2 import Align_base
-
-
-def get_colorpoints(pcd, reverse=False):
-  # Get sample colors for train ================================
-  avene_colors = _get_pcd("../OB_def/avene_sample.pcd", colors=True)["colors"]        	# Avenes samples
-  cidonija_colors = _get_pcd("../OB_def/cidonija_sample.pcd", colors=True)["colors"] 	# cidonijas samples
-  NOTOB_colors4 = _get_pcd("../OB_def/Not_object_main.pcd", colors=True)["colors"] 	# Not a object samles
-  NOTOB_colors2 = _get_pcd("../OB_def/NO.pcd", colors=True)["colors"]			# Not a object samples 2
-
-  # Get pointcloud samples =====================================
-  pcd_points = np.asarray(pcd.points)
-  pcd_colors = np.asarray(pcd.colors)
-
-  # Get a Mean's of all pointcloud's colors ====================
-  avene_mean = np.mean(avene_colors, axis=0)
-  cidonija_mean = np.mean(cidonija_colors, axis=0)
-  NOTOB_mean4 = np.mean(NOTOB_colors4, axis=0)
-  NOTOB_mean2 = np.mean(NOTOB_colors2, axis=0)
-  # Get a Norm's to an object mean and not a object sample mean ========
-  avene_Norm = np.linalg.norm(pcd_colors - avene_mean, axis=1)
-  cidonija_Norm = np.linalg.norm(pcd_colors - cidonija_mean, axis=1)
-  NOTOB_Norm4 = np.linalg.norm(pcd_colors - NOTOB_mean4, axis=1)
-  NOTOB_Norm2 = np.linalg.norm(pcd_colors - NOTOB_mean2, axis=1)
-  # 'for_test' basicly is a test, this is a avenes or cidonijas samples ====
-  for_test = (avene_Norm < NOTOB_Norm4) & (avene_Norm < 0.5)
-  # Get object points or get rid of object points ======================
-  if reverse:
-    pcd_points = pcd_points[(avene_Norm > NOTOB_Norm4) & (avene_Norm > 0.5)]
-    pcd_colors = pcd_colors[(avene_Norm > NOTOB_Norm4) & (avene_Norm > 0.5)]
-    # Get more points if we are looking for cidonijas===================
-    if np.size(for_test) < 50:
-      p_1 = pcd_points[(cidonija_Norm > NOTOB_Norm4) & (cidonija_Norm > NOTOB_Norm2) & (cidonija_Norm > 0.5)]
-      pcd_points = np.vstack((pcd_points, p_1))
-      p_1 = pcd_colors[(cidonija_Norm > NOTOB_Norm4) & (cidonija_Norm > NOTOB_Norm2) & (cidonija_Norm > 0.5)]
-      pcd_colors = np.vstack((pcd_colors, p_1))
-    else:
-      pass
-  else:
-    pcd_points = pcd_points[(avene_Norm < NOTOB_Norm4) & (avene_Norm < 0.5)]
-    pcd_colors = pcd_colors[(avene_Norm < NOTOB_Norm4) & (avene_Norm < 0.5)]
-    if np.size(for_test) < 50:
-      p_1 = pcd_points[(cidonija_Norm < NOTOB_Norm4) & (cidonija_Norm < NOTOB_Norm2) & (cidonija_Norm < 0.5)]
-      pcd_points = np.vstack((pcd_points, p_1))
-      p_1 = pcd_colors[(cidonija_Norm < NOTOB_Norm4) & (cidonija_Norm < NOTOB_Norm2) & (cidonija_Norm < 0.5)]
-      pcd_colors = np.vstack((pcd_colors, p_1))
-    else:
-      pass
-  return pcd_points, pcd_colors
 
 def get_points(points, Z_VALUE):
   max_z = np.amax(points, where=[False, False, True], initial = -np.inf)
@@ -109,14 +61,18 @@ def get_BoBox(obj_points):
   return BB
 
 def main():
-  pcd = _get_pcd("../par_10.pcd", pcd=True, points=True, colors=True)
+  pcd = _get_pcd("../par_10.pcd", pcd=True)["pcd"]
   # Get pcd, pcd points and pcd colors ===============
-  points = pcd["points"]
-  colors = pcd["colors"]
-  pcd = pcd["pcd"]  # <-- 4. rotate this pcd.
+
   # pcd rotation =====================================
   pcd = Align_base(pcd)
+  points = np.asarray(pcd.points)
   # Def bounding box variables =======================
+  y_max = Find_corner(np.asarray(pcd.points), 100000)["y_max"]
+  points = points[points[:, 2] < y_max[2]-5]
+  breakpoint()
+  show_pcd(points, vector=True)
+  quit()
   BB = {}
   BB_nr = 0
   count = 0
@@ -133,8 +89,9 @@ def main():
 		analīze pēc augstuma
   """
   # Get object points ===============================
-  Z_VALUE = Find_corner(np.asarray(pcd.points), y_max=True)["y_max"][2]
+  Z_VALUE = Find_corner(np.asarray(pcd.points), 100000)["y_max"][2]
   OB = get_points(points, Z_VALUE)
+  quit()
   # Is it an object? ================================
   Is = Is_object(OB)
   # If it is an object, get bounding box ============
